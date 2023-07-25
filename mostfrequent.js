9b5bb4c9b901dbf49d118d7110239119d415dbfd
RianOntu <rianontu13@gmail.com>
2023-07-25 17:50:01 +0600

    solved most frequent number problem

---
+++ mostfrequent.js
@@ -0,0 +1,23 @@
+function findMostFrequentElement(arr) {
+    const frequencyMap = {};
+    let mostFrequentElement;
+    let maxFrequency = 0;
+  
+    for (let i = 0; i < arr.length; i++) {
+      const currentElement = arr[i];
+      frequencyMap[currentElement] = (frequencyMap[currentElement] || 0) + 1;
+  
+      if (frequencyMap[currentElement] > maxFrequency) {
+        maxFrequency = frequencyMap[currentElement];
+        mostFrequentElement = currentElement;
+      }
+    }
+  
+    return mostFrequentElement;
+  }
+  
+  
+  const inputArray = [3, 5, 2, 5, 3, 3, 1, 4, 5];
+  const outputElement = findMostFrequentElement(inputArray);
+  console.log(outputElement); 
+  
